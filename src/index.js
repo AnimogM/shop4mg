71df3cb836ddfa01d5736b63d609e39bf3b49f26
Gomina Maryam <80972735+AnimogM@users.noreply.github.com>
2022-05-19 15:43:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { extendTheme, ChakraProvider } from "@chakra-ui/react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import CartProvider from "./context/CartContext";
 
